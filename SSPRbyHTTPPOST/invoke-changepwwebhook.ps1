66202b2ad8538adfe09e8703290422eb645fdc75
mrallentam <47073620+mrallentam@users.noreply.github.com>
2021-08-10 22:32:04 -0700

    Add files via upload

---
+++ SSPRbyHTTPPOST/invoke-changepwwebhook.ps1
@@ -0,0 +1,43 @@
+﻿Param (
+	[Parameter(Mandatory = $true,
+			   ParameterSetName = "XXUSERNAME")]
+	[String[]]$XXUSERNAME,
+	
+	[Parameter(Mandatory = $true,
+			   ParameterSetName = "XXPASSWORD")]
+	[String[]]$XXPASSWORD,
+	
+	[Parameter(Mandatory = $true,
+			  ParameterSetName = "NEWESAEPASSWORD")]
+	[String[]]$NEWESAEPASSWORD
+	
+	)
+
+#Ping authentication
+$pingurl=
+$pinguser = $XXusername
+$pingpass = $XXpassword
+$secpingpasswd = ConvertTo-SecureString $pingpass -AsPlainText -Force
+$pingcredential = New-Object System.Management.Automation.PSCredential($pinguser, $secpingpasswd)
+$authorized = Invoke-RestMethod $pingurl -Credential $pingcredential
+
+if ($authorized -eq $false) {
+Write-output "User is not Authorized to change password"
+exit}
+
+else {
+$USER = "z-"+$XXUSERNAME
+[Byte[]]$key = (1 .. 16)
+$PW = $NEWRDSPASSWORD | ConvertTo-SecureString -AsPlainText -Force
+$Password = $pw | ConvertFrom-SecureString -key $key
+$uri = "HTTP://URI"
+$data = @(
+		@{ User = "$user"; Password = "$password" }
+		)
+		$body = ConvertTo-Json -InputObject $data
+		$header = @{ message = "changepasswordtest" }
+		$response = Invoke-WebRequest -Method Post -Uri $uri -Body $body -Headers $header
+		#$jobid = (ConvertFrom-Json ($response.Content)).jobids[0]
+		
+	}
+	
---
+++ SSPRbyHTTPPOST/webhook.ps1
@@ -0,0 +1,77 @@
+﻿
+##############################
+# Define output stream type
+[OutputType([object])]
+
+# Define runbook input params
+Param
+(
+        [Parameter(Mandatory=$true)]
+        [pscredential] 
+        $xxxusername,
+
+        [Parameter(Mandatory=$true)]
+        [pscredential] 
+        $xxxpassword,
+
+        [Parameter(Mandatory=$true)]
+        [PSCredential] 
+        $NewRDSPassword,
+)
+
+$con = Get-AutomationConnection -Name "AzureRunAsConnection"
+
+Connect-AzAccount `
+    -ServicePrincipal `
+    -Tenant $con.TenantId `
+    -ApplicationId $con.ApplicationId `
+    -CertificateThumbprint $con.CertificateThumbprint 
+
+#invoke webrequest for 
+$authenticated = invoke 
+
+if ($authenticated -eq $false){
+write-output "Error User not verified through xxx"
+exit   
+}
+else
+{
+                                                               
+$guser="z-"+$xxxusername
+
+mkdir c:\temp\pwinfo
+
+$guser|out-file c:\temp\pwinfo\user.txt
+$file="c:\temp\pwinfo\pw.txt"
+[Byte[]] $key = (1..16)
+$Password =  $NewRDSPassword| ConvertTo-SecureString -AsPlainText -Force
+$Password | ConvertFrom-SecureString -key $key | Out-File $File
+start-sleep -seconds 5
+
+
+Start-Process -verb runas PowerShell.exe -argumentlist '-command invoke-command -scriptblock {
+$action = New-ScheduledTaskAction -Execute PowerShell.exe -argument "{
+$rfuser=get-content c:\temp\pwinfo\user.txt
+[Byte[]] $key = (1..16)
+$pwd=Get-Content c:\temp\pwinfo\pw.txt|ConvertTo-SecureString -Key $key
+Set-ADAccountPassword -Identity $rfuser -Reset -NewPassword $pwd 
+}"
+$trigger= New-ScheduledTaskTrigger -At 12am -Once
+Register-ScheduledTask -Action $action -TaskName ChangePassword -RunLevel highest -Trigger $trigger
+}'
+start-sleep -seconds 5
+
+Start-Process -verb runas PowerShell.exe -argumentlist '-command invoke-command -scriptblock {
+$principal = New-ScheduledTaskPrincipal -UserId domain\gmsaaccount -LogonType Password -RunLevel highest
+Set-ScheduledTask ChangePassword -Principal $principal
+}'
+start-sleep -seconds 5
+
+Start-Process -verb runas PowerShell.exe -argumentlist '-command Start-ScheduledTask -TaskName ChangePassword'
+start-sleep -seconds 5
+
+Start-Process -verb runas PowerShell.exe -argumentlist '-command Unregister-ScheduledTask -TaskName ChangePassword -Confirm:$false'
+start-sleep -seconds 5
+
+Start-Process -verb runas PowerShell.exe -argumentlist '-command remove-item -path c:\temp\pwinfo -recurse -force'
+}
